13480a33a23bb32b4432f4f7b99bb589e67abfc5
Zachary Boyd <zachary@zacharyboyd.nyc>
2020-12-09 15:02:52 -0500

    inital

---
+++ main.js
@@ -0,0 +1,157 @@
+require('dotenv').config();
+
+const { app, BrowserWindow, Menu } = require('electron');
+const contextMenu = require('electron-context-menu');
+
+contextMenu();
+
+let urls = {
+    'docker-dev': 'https://dev-etomon.com',
+    'local': 'http://localhost:4200',
+    'production': 'https://etomon.com'
+}
+
+let mode = process.env.MODE || 'production';
+let siteUri = process.env.SITE_URI || urls[mode];
+let isDev = mode !== 'production';
+
+const isMac = process.platform === 'darwin';
+let win;
+const template = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    }] : []),
+    // { role: 'fileMenu' }
+    {
+        label: 'File',
+        submenu: [
+            isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+    },
+    // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            ...(isMac ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                    label: 'Speech',
+                    submenu: [
+                        { role: 'startSpeaking' },
+                        { role: 'stopSpeaking' }
+                    ]
+                }
+            ] : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+            ])
+        ]
+    },
+    // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forceReload' },
+            { role: 'togglefullscreen' },
+            { type: 'separator' },
+            { role: 'resetZoom' },
+            { role: 'zoomIn' },
+            { role: 'zoomOut' },
+            { type: 'separator' },
+            (
+                isDev ?
+                    { role: 'toggleDevTools' } : null
+            )
+        ].filter(Boolean)
+    },
+    // { role: 'windowMenu' }
+    {
+        label: 'Window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'zoom' },
+            ...(isMac ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+            ] : [
+                { role: 'close' }
+            ])
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Help',
+                click: async () => {
+                    return win.loadURL(`${siteUri}/help`);
+                }
+            },
+            {
+                label: 'Contact Etomon',
+                click: async () => {
+                   return win.loadURL(`${siteUri}/contact-us`);
+                }
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            enableRemoteModule: false
+        },
+        title: 'Etomon',
+        icon: __dirname + '/assets/icon'
+    })
+
+    win.maximize();
+
+    win.loadURL(siteUri);
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
