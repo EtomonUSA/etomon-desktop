bb1d51bc08c6437e5cf7f3a738fb5e681b9853be
Zachary Boyd <zachary@zacharyboyd.nyc>
2020-12-26 01:47:55 -0500

    uses normal bilibili iframe if not using electron

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -rf out
-electron-forge publish --targets @electron-forge/maker-zip --platform linux
-electron-forge publish --targets @electron-forge/maker-zip --platform darwin
+npx electron-forge publish --targets @electron-forge/maker-zip --platform linux
+npx  electron-forge publish --targets @electron-forge/maker-zip --platform darwin
 #electron-forge make --targets @electron-forge/maker-zip --platform win32
 
-electron-forge publish --targets @electron-forge/maker-deb --platform linux
+npx electron-forge publish --targets @electron-forge/maker-deb --platform linux
 
-electron-forge publish --targets @electron-forge/maker-dmg --platform darwin
+npx electron-forge publish --targets @electron-forge/maker-dmg --platform darwin
 
 #electron-forge make --targets @electron-forge/maker-squirrel --platform win32
--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     "packagerConfig": {
-        "icon": "assets/icon",
         "platform": "all",
         executableName: "etomon-desktop"
     },
